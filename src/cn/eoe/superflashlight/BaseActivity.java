9693ee506a4e73a1e9fdbd615ed9d45f7f7c23fb
brkjwtl <455079505@qq.com>
2017-07-17 09:09:16 +0800

    04 主界面布局

--- src/cn/eoe/superflashlight/BaseActivity.java
+++ src/cn/eoe/superflashlight/BaseActivity.java
@@ -4,21 +4,40 @@ import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class BaseActivity extends Activity {
+	protected enum UIType {
+		UI_TYPE_MAIN, UI_TYPE_FLASHLIGHT, UI_TYPE_WARNINGLIGHT, UI_TYPE_MORSE, UI_TYPE_BLUB, UI_TYPE_COLOR, UI_TYPE_POLICE, UI_TYPE_SETTINGS
+	}
+
 	protected ImageView mImageViewFlashlight;
 	protected ImageView mImageViewFlashlightController;
 	protected Camera mCamera;
 	protected Parameters mParameters;
 
+	protected FrameLayout mUIFlashlight;
+	protected LinearLayout mUIMain;
+	
+	protected UIType mCurrentUIType = UIType.UI_TYPE_FLASHLIGHT;
+	protected UIType mLastUIType = UIType.UI_TYPE_FLASHLIGHT;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		mUIFlashlight = (FrameLayout) findViewById(R.id.framelayout_flashlight);
+		mUIMain = (LinearLayout) findViewById(R.id.linearlayout_main);
 		mImageViewFlashlight = (ImageView) findViewById(R.id.imageview_flashlight);
 		mImageViewFlashlightController = (ImageView) findViewById(R.id.imageview_flashlight_controller);
 
+	}
+
+	protected void hideAllUI() {
+		mUIMain.setVisibility(View.GONE);
+		mUIFlashlight.setVisibility(View.GONE);
 	}
 }
