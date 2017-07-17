5660006996fed09fe009462b7707daea43f8b80a
brkjwtl <455079505@qq.com>
2017-07-17 09:43:46 +0800

    09 彩色灯（二）

--- src/cn/eoe/superflashlight/ColorLight.java
+++ src/cn/eoe/superflashlight/ColorLight.java
@@ -1,11 +1,12 @@
 package cn.eoe.superflashlight;
 
+import cn.eoe.superflashlight.ColorPickerDialog.OnColorChangedListener;
 import cn.eoe.superflashlight.widget.HideTextView;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
-public class ColorLight extends Bulb {
+public class ColorLight extends Bulb  implements OnColorChangedListener{
 	protected int mCurrentColorLight = Color.RED;
 	protected HideTextView mHideTextViewColorLight;
 
@@ -15,9 +16,16 @@ public class ColorLight extends Bulb {
 		mHideTextViewColorLight = (HideTextView)findViewById(R.id.textview_hide_color_light);
 	}
 	
+	@Override
+	public void colorChanged(int color) {
+		mUIColorLight.setBackgroundColor(color);
+		mCurrentColorLight = color;
+		
+	}
+
 	public void onClick_ShowColorPicker(View view)
 	{
-		
+		new ColorPickerDialog(this, this, Color.RED).show();
 	}
 
 }
